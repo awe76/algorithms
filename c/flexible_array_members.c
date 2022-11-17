b94cd052b5701799571504f4af4a2218e1665d0d
Andrei Silchankau <andrei.silchankau@hqo.co>
2022-11-17 22:01:27 +0100

    realloc memory for FAM, dynamic memory reallocation

--- c/flexible_array_members.c
+++ c/flexible_array_members.c
@@ -4,29 +4,44 @@
 // flexible array members (FAM) sample 
 // see more details here: https://www.scaler.com/topics/c/dynamic-array-in-c/ 
 typedef struct vector {
+    int cap;
     int size;
     int arr[];
 } vector;
 
-vector* create_vector(int size) {
-    vector* r = malloc(sizeof(vector*) + size * sizeof(int));
-    r->size = size;
+vector* create_vector(int cap) {
+    vector* r = malloc(sizeof(vector*) + cap * sizeof(int));
+    r->size = 0;
+    r->cap = cap;
     return r;
+}
+
+vector* push(vector* v, int item) {
+    if (v->size == v->cap) {
+        int cap = 2 * v->cap;
+        int size = v->size;
+        v = realloc(v, sizeof(vector*) + cap * sizeof(int));
+        v->cap = cap;
+        v->size = size;
+    }
+    v->arr[v->size++] = item;
+    return v;
 }
 
 int main() {
     vector* v = create_vector(5);
+    int max = 3 * v->cap;
 
-    for (int i = 0; i < v->size; i++) {
-        v->arr[i] = i;
+    for (int i = 0; i < max; i++) {
+        v = push(v, i);
     }
 
-    for (int i = 0; i < v->size; i++) {
+    for (int i = 0; i < max; i++) {
         printf("%d ", v->arr[i]);
     }
 
-    printf("\nSize:");
-    printf("\n%d", v->size);
+    printf("\nCap:%d", v->cap);
+    printf("\nSize:%d", v->size);
 
     free(v);
     return 0;
