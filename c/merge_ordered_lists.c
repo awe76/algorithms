a11af360cdf83c3296be7a9fbf0b30587e880cf6
Andrei Silchankau <andrei.silchankau@hqo.co>
2022-11-20 23:29:13 +0100

    implement merge sort

--- c/merge_ordered_lists.c
+++ c/merge_ordered_lists.c
@@ -25,14 +25,60 @@ int merge(int a[], int n, int b[], int m, int c[]) {
     return m + n;
 }
 
-int main() {
+// copy range source[from, to) to target[0, (from - to))
+int copy(int source[], int target[], int from, int to) {
+    for (int i = 0; i < to - from; i++) {
+        target[i] = source[from + i];
+    }
+
+    return to - from;
+}
+
+// sort target[n] array with merge sort
+void merge_sort(int target[], int n) {
+    int m = n / 2;
+    if (m > 0) {
+        int a[m];
+        int b[n - m];
+
+        copy(target, a, 0, m);
+        copy(target, b, m, n);
+
+        merge_sort(a, m);
+        merge_sort(b, n - m);
+
+        merge(a, m, b, n - m, target);
+    }
+}
+
+void test_merge() {
     int a[] = {21, 28, 35, 40, 61, 75};
     int b[] = {16, 25, 47, 54};
     int c[10];
 
+    printf("test merge...\n");
     int r = merge(a, 6, b, 4, c);
+
     for (int i = 0; i < r; i++) {
         printf("%d ", c[i]);
     }
+}
+
+void test_merge_sort() {
+    int target[] = {21, 28, 35, 40, 61, 75, 16, 25, 47, 54};
+    int n = 10;
+
+    printf("test merge sort...\n");
+    merge_sort(target, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", target[i]);
+    }
+
+    // 16 21 25 28 35 40 47 54 61 75
+}
+
+int main() {
+    // test_merge();
+    test_merge_sort();
     return 0;
 }
